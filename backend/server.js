53ddc3c3dba9b1f85c3b1a292dd724ac81da36ea
brand310 <brandonsandoval0@gmail.com>
2023-02-14 21:35:26 -0800

    server.js updated and dried

--- backend/server.js
+++ backend/server.js
@@ -3,29 +3,36 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-//
+//STRICT SET-UP
 mongoose.set("strictQuery", false);
 //mongoose.set("strictQuery", true);
 
 //MIDDLEWARE
 require("dotenv").config();
+
 const app = express();
 app.use(express.json());
+
 app.use(cors());
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+
+
+
+//MONGO SETUP
 mongoose.connect(
   process.env.MONGO_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
-    console.log("connected to mongo on: ", process.env.MONGO_URI);
+    console.log("Connected to MongoDB on:", process.env.MONGO_URI);
   }
 );
 
+//ROUTES
+app.use(require("./routes/index"));
+
+app.use(require("./routes/todo"));
+
 //PORT
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`server is running on port: ${port}`);
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`)
 });
