36106e30ec8efabc5c33af01f76a2fc76b78c483
brand310 <brandonsandoval0@gmail.com>
2023-02-15 17:04:04 -0800

    todo Schema has been updated

--- backend/models/todo.js
+++ backend/models/todo.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const todoSchema = new mongoose.Schema({
     todo: {
         type: String,
         required: true,
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    },
+    timestamp: {
+        type: String,
+        default: Date.now()
     }
 })
 
-const todo = mongoose.model("todo", todoSchema);
-module.exports = todo
+const Todo = mongoose.model("Todo", todoSchema);
+module.exports = Todo;
