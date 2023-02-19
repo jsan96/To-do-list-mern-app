a9c7cbc8fa5d21a4bf45c2b9c3ff68cba4bdc2d7
craftyaj <craftyaj11@gmail.com>
2023-02-19 13:32:36 -0800

    styles updated

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,40 +1,19 @@
+import NavBar from "scenes/NavBar";
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import HomePage from './HomePage';
-import FrontPage from './FrontPage';
-import ProfilePage from './ProfilePage';
-import NavBar from "./NavBar";
-
-function NavBar() {
-  return <nav className="nav">
-      <a href="'/" className="page-title">Life List</a>
-      <ul>
-          <li>
-              <a href="/login">Log In</a>
-          </li>
-          <li>
-              <button href="/sign-up">Sign-Up</button>
-          </li>
-      </ul>
-  </nav>
-}
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from 'scenes/HomePage';
 
 function App() {
   return (
     <Router>
       <div>
         <NavBar />
-        <Switch>
           <Route exact path="/">
             <HomePage />
           </Route>
-          <Route exact path="/frontpage">
-            <FrontPage />
+          <Route exact path="/HomePage">
+            <HomePage />
           </Route>
-          <Route exact path="/profile">
-            <ProfilePage name="John Doe" bio="I'm a web developer" location="New York" />
-          </Route>
-        </Switch>
       </div>
     </Router>
   );
---
+++ frontend/client/src/scenes/RegisterPage/index.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import Navbar from "scenes/NavBar"; 
