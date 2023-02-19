3c419dd45c7724ec5ee68085410145745ff5db3f
craftyaj <craftyaj11@gmail.com>
2023-02-19 10:11:32 -0800

    color palette established

---
+++ frontend/client/src/theme.js
@@ -0,0 +1,19 @@
+export const colorTokens = {
+    grey: {
+        0:"#656565",
+        10: "808782",
+    },
+    white: {
+        0: "FFFFFF",
+    },
+    black: {
+        0: "000000",
+    },
+    primary: {
+        0: "D1FFD7",
+        10: "B3FFB3",
+        50: "A6D3A0",
+    },
+};
+
+// mui theme settings 
