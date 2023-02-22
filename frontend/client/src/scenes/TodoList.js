b3f3ab59bc4e5fd4ba3f3908974a47aa2da5bb62
brand310 <brandonsandoval0@gmail.com>
2023-02-21 19:55:28 -0800

    CSS updated

--- frontend/client/src/scenes/TodoList.js
+++ frontend/client/src/scenes/TodoList.js
@@ -1,2 +1,16 @@
-import NavBar from "scenes/NavBar";
-import React from 'react';
+//import React, from 'react';
+
+function TodoPage() {
+  return (
+    <header>
+      <h1 className="Todo1">To Do List</h1>
+      <form className= "to-do-list">
+        <input type="text" className= "to-do-list-input" placeholder="What Do You Need To Remember..."/>
+        <input type="text" className= "to-do-list-input" placeholder="Due Date" />
+        <input type="submit" className= "to-do-list-input" value="Post" />
+      </form>
+    </header>
+  );
+}
+
+export default TodoPage;
